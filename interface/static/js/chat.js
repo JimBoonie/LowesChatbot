e1ca3a652bee03ddf2955de67916510c7dec4e92
Mason McGough <mcgough.mason@gmail.com>
2019-07-11 13:07:00 -0700

    Move page js into files.

---
+++ interface/static/js/chat.js
@@ -0,0 +1,51 @@
+var addMessageToLog = function(message, author) {
+    if (message == '') {
+        return;
+    }
+    
+    var msg_count = messageHistory.length;
+    var message = {
+        id: msg_count,
+        text: message,
+        author: author,
+        datetime: 'December 25, 1995 23:15:20'
+    }
+    messageHistory.push(message);
+}
+
+var submitMessage = function() {
+    var message_element = document.getElementById('chat-input');
+    addMessageToLog(message_element.value, 'author-user');
+    message_element.value = '';
+}
+
+var scrollChatToBottom = function() {
+    var chat_log = document.getElementById('chat-log');
+    chat_log.scrollTop = chat_log.scrollHeight;
+}
+
+Vue.component('message-item', {
+    props: ['message'],
+    template: '<div class="message">' +
+    '<div class="message-content">{{ message.text }}</div>' + 
+    '<div class="message-datetime">{{ message.datetime }}</div>' +
+    '</div>'
+})
+
+var messageHistory = [
+    { id: 0, text: 'Yes, can I have a dozen red roses please.', author: 'author-user', datetime: 'December 25, 1995 23:15:20' },
+    { id: 1, text: 'Oh hai Johnny, I didn\'t know it was you.', author: 'author-other', datetime: 'December 25, 1995 23:15:20' },
+    { id: 2, text: 'That\'s me. How much is it?', author: 'author-user', datetime: 'December 25, 1995 23:15:20' },
+    { id: 3, text: 'That\'ll be $18.', author: 'author-other', datetime: 'December 25, 1995 23:15:20' },
+    { id: 4, text: 'Here you go, keep the change, hai doggy.', author: 'author-user', datetime: 'December 25, 1995 23:15:20' },
+    { id: 5, text: 'You\'re my favorite customer.', author: 'author-other', datetime: 'December 25, 1995 23:15:20' },
+    { id: 6, text: 'Thanks a lot. Bye.', author: 'author-user', datetime: 'December 25, 1995 23:15:20' }
+];
+
+var chat = new Vue({
+    el: '#chat-log',
+    data: {
+        messages: messageHistory
+    },
+    updated: scrollChatToBottom
+})
